3337f7dd4b03542afe1daf7a52096248e73eac9a
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-09-07 14:28:46 +0100

    Set up surface and scaffold

--- app/src/main/java/com/example/androidcomposeguideapp/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposeguideapp/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidcomposeguideapp.ui.theme.AndroidComposeGuideAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidComposeGuideAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GuideApp(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GuideApp()
                 }
             }
         }
@@ -30,10 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun GuideApp(modifier: Modifier = Modifier) {
+private fun GuideApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GuideList(contentPadding = it)
+    }
+}
+
+@Composable
+private fun GuideList(contentPadding: PaddingValues = PaddingValues(0.dp)) {
     Text(
-        text = "Hello!",
-        modifier = modifier
+        text = "Hello!"
     )
 }
 
