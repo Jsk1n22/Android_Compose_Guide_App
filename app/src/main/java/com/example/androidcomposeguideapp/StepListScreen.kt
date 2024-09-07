0266c196deb7742c81d06a5674f67d2a00d1bb6d
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-09-07 22:19:20 +0100

    Improve card modifiers

--- app/src/main/java/com/example/androidcomposeguideapp/StepListScreen.kt
+++ app/src/main/java/com/example/androidcomposeguideapp/StepListScreen.kt
@@ -3,13 +3,18 @@ package com.example.androidcomposeguideapp
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -17,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androidcomposeguideapp.model.Step
 import com.example.androidcomposeguideapp.model.StepRepository
+import com.example.androidcomposeguideapp.ui.theme.Shapes
 import com.example.androidcomposeguideapp.ui.theme.Typography
 
 @Composable
@@ -42,6 +48,7 @@ private fun StepItem(
         Column(
             modifier
                 .padding(dimensionResource(id = R.dimen.padding_medium))
+                .fillMaxWidth()
         ) {
             Text(
                 text = stringResource(id = step.stepName),
@@ -50,8 +57,12 @@ private fun StepItem(
             Image(
                 painter = painterResource(id = step.stepImg),
                 contentDescription = null,
+                contentScale = ContentScale.Fit,
                 modifier = Modifier
                     .padding(vertical = dimensionResource(id = R.dimen.padding_small))
+                    .height(200.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .clip(Shapes.small)
             )
             Text(
                 text = stringResource(id = step.stepInfo),
