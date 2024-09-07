3527c422ed22abb85fec2636525ddb81ec6526a2
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-09-07 20:22:10 +0100

    Add TopAppBar and Dark/Light Previews

--- app/src/main/java/com/example/androidcomposeguideapp/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposeguideapp/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.androidcomposeguideapp.ui.theme.AndroidComposeGuideAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,16 +38,28 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun GuideApp() {
     Scaffold(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopBar() }
     ) {
         GuideList(contentPadding = it)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun GuideList(contentPadding: PaddingValues = PaddingValues(0.dp)) {
-    Text(
-        text = "Hello!"
+private fun TopBar(
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name)
+            )
+        },
     )
 }
 
@@ -54,8 +68,19 @@ private fun GuideList(contentPadding: PaddingValues = PaddingValues(0.dp)) {
     showSystemUi = true
 )
 @Composable
-fun GreetingPreview() {
-    AndroidComposeGuideAppTheme {
+fun GreetingDarkPreview() {
+    AndroidComposeGuideAppTheme(darkTheme = true) {
+        GuideApp()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingLightPreview() {
+    AndroidComposeGuideAppTheme(darkTheme = false) {
         GuideApp()
     }
 }
