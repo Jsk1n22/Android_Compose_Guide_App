40f68ac39bbcbd3b080c4d0ea81662b714c1c6a2
Jsk1n22 <jsk1n22@soton.ac.uk>
2024-09-08 21:32:11 +0100

    Add the remaining steps

--- app/src/main/java/com/example/androidcomposeguideapp/model/StepRepository.kt
+++ app/src/main/java/com/example/androidcomposeguideapp/model/StepRepository.kt
@@ -3,7 +3,7 @@ package com.example.androidcomposeguideapp.model
 import com.example.androidcomposeguideapp.R
 
 object StepRepository {
-    val steps = listOf<Step>(
+    val steps = listOf(
         Step(
             stepName = R.string.step_one_name,
             stepInfo = R.string.step_one_info,
@@ -13,6 +13,126 @@ object StepRepository {
             stepName = R.string.step_two_name,
             stepInfo = R.string.step_two_info,
             stepImg = R.drawable.configure_project_2,
+        ),
+        Step(
+            stepName = R.string.step_three_name,
+            stepInfo = R.string.step_three_info,
+            stepImg = R.drawable.remove_greeting_3
+        ),
+        Step(
+            stepName = R.string.step_four_name,
+            stepInfo = R.string.step_four_info,
+            stepImg = R.drawable.set_up_surface_4
+        ),
+        Step(
+            stepName = R.string.step_five_name,
+            stepInfo = R.string.step_five_info,
+            stepImg = R.drawable.set_up_scaffold_5
+        ),
+        Step(
+            stepName = R.string.step_six_name,
+            stepInfo = R.string.step_six_info,
+            stepImg = R.drawable.create_colour_scheme_6
+        ),
+        Step(
+            stepName = R.string.step_seven_name,
+            stepInfo = R.string.step_seven_info,
+            stepImg = R.drawable.add_colors_7
+        ),
+        Step(
+            stepName = R.string.step_eight_name,
+            stepInfo = R.string.step_eight_info,
+            stepImg = R.drawable.add_light_dark_schemes_8
+        ),
+        Step(
+            stepName = R.string.step_nine_name,
+            stepInfo = R.string.step_nine_info,
+            stepImg = R.drawable.add_fonts_9
+        ),
+        Step(
+            stepName = R.string.step_ten_name,
+            stepInfo = R.string.step_ten_info,
+            stepImg = R.drawable.import_strings_10
+        ),
+        Step(
+            stepName = R.string.step_eleven_name,
+            stepInfo = R.string.step_eleven_info,
+            stepImg = R.drawable.import_dimens_11
+        ),
+        Step(
+            stepName = R.string.step_twelve_name,
+            stepInfo = R.string.step_twelve_info,
+            stepImg = R.drawable.import_shapes_12
+        ),
+        Step(
+            stepName = R.string.step_thirteen_name,
+            stepInfo = R.string.step_thirteen_info,
+            stepImg = R.drawable.import_images_13
+        ),
+        Step(
+            stepName = R.string.step_fourteen_name,
+            stepInfo = R.string.step_fourteen_info,
+            stepImg = R.drawable.make_app_logo_14
+        ),
+        Step(
+            stepName = R.string.step_fifteen_name,
+            stepInfo = R.string.step_fifteen_info,
+            stepImg = R.drawable.make_model_class_15
+        ),
+        Step(
+            stepName = R.string.step_sixteen_name,
+            stepInfo = R.string.step_sixteen_info,
+            stepImg = R.drawable.make_class_list_16
+        ),
+        Step(
+            stepName = R.string.step_seventeen_name,
+            stepInfo = R.string.step_seventeen_info,
+            stepImg = R.drawable.make_screen_class_17
+        ),
+        Step(
+            stepName = R.string.step_eighteen_name,
+            stepInfo = R.string.step_eighteen_info,
+            stepImg = R.drawable.make_top_bar_18
+        ),
+        Step(
+            stepName = R.string.step_nineteen_name,
+            stepInfo = R.string.step_nineteen_info,
+            stepImg = R.drawable.make_previews_19
+        ),
+        Step(
+            stepName = R.string.step_twenty_name,
+            stepInfo = R.string.step_twenty_info,
+            stepImg = R.drawable.make_template_20
+        ),
+        Step(
+            stepName = R.string.step_twentyone_name,
+            stepInfo = R.string.step_twentyone_info,
+            stepImg = R.drawable.make_grid_21
+        ),
+        Step(
+            stepName = R.string.step_twentytwo_name,
+            stepInfo = R.string.step_twentytwo_info,
+            stepImg = R.drawable.use_modifiers_22
+        ),
+        Step(
+            stepName = R.string.step_twentythree_name,
+            stepInfo = R.string.step_twentythree_info,
+            stepImg = R.drawable.import_icons_23
+        ),
+        Step(
+            stepName = R.string.step_twentyfour_name,
+            stepInfo = R.string.step_twentyfour_info,
+            stepImg = R.drawable.add_buttons_24
+        ),
+        Step(
+            stepName = R.string.step_twentyfive_name,
+            stepInfo = R.string.step_twentyfive_info,
+            stepImg = R.drawable.growth_animations_25
+        ),
+        Step(
+            stepName = R.string.step_twentysix_name,
+            stepInfo = R.string.step_twentysix_info,
+            stepImg = R.drawable.colour_animations_26
         ),
     )
 }
